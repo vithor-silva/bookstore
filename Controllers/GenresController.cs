303d18bfe6eb3f4ba891f8455c7a6dda633b367b
VITHOR MARTINS SILVA <vithor.silva@alunos.sc.senac.br>
2024-11-04 15:21:54 -0300

    feat: Add connection to database

--- Controllers/GenresController.cs
+++ Controllers/GenresController.cs
@@ -1,26 +1,21 @@
-﻿using Bookstore.Models;
+﻿using Bookstore.Data;
+using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bookstore.Controllers
 {
     public class GenresController : Controller
     {
+        private readonly BookstoreContext _context;
+
+        public GenresController(BookstoreContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            List<Genre> genres = new List<Genre>
-            {
-                new Genre
-                {
-                    Id = 1,
-                    Name = "Romance"
-                },
-                new Genre
-                {
-                    Id =2,
-                    Name = "Fantasia"
-                }
-            };
-            return View(genres);
+            return View(_context.Genres.ToList());
         }
     }
 }
